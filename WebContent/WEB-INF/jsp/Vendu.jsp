7280a28385faa7877525a16dfd050d51c46b6ced
MaRiOn202 <77493223+MaRiOn202@users.noreply.github.com>
2021-12-08 09:49:01 +0100

    VERSION MISE à JOUR  Vendu.jsp
    VERSION MISE à JOUR
    Vendu.jsp

--- WebContent/WEB-INF/jsp/Vendu.jsp
+++ WebContent/WEB-INF/jsp/Vendu.jsp
@@ -7,6 +7,6 @@
 <title>Vendu</title>
 </head>
 <body>
-	
+	<h1>Hello</h1>
 </body>
 </html>
--- src/fr/eni/zylim/encheres/servlet/VendreArticleServlet.java
+++ src/fr/eni/zylim/encheres/servlet/VendreArticleServlet.java
@@ -27,7 +27,7 @@ public class VendreArticleServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		getServletContext().getRequestDispatcher("/WEB-INF/jsp/Vendu.jsp").forward(request, response);
 	}
 
 	/**
