446c4a3d4a19204c4b4f9d8f403de6af717cf5a3
BelkZak <belkhatirzakaria1@gmail.com>
2021-12-08 17:01:25 +0100

    mise ajour utilisateur consulter profile
    prototip creation utilisateur dao /factory/impl

--- src/fr/eni/zylim/encheres/dal/UtilisateurDAOFactory.java
+++ src/fr/eni/zylim/encheres/dal/UtilisateurDAOFactory.java
@@ -1,9 +1,17 @@
 package fr.eni.zylim.encheres.dal;
 
+
 import fr.eni.zylim.encheres.dal.jdbc.UtilisateurDAOJdbcImpl;
 
 public class UtilisateurDAOFactory {
-	
+
+	public static UtilisateurDAO getUtilisateurDAO() {
+		// TODO Auto-generated method stub
+		return new UtilisateurDAOJdbcImpl();
+
+	}
+
+
 //	/**
 //	 * Fournit une instance d'une classe
 //	 * se comportant comme un RepasDAO
