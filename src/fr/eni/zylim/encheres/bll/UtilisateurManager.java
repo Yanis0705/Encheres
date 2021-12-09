a8331fab3816b7b28e5389370b0c2c9b33362311
Yanis Chaigneau <yanis.chaigneau.yc2@gmail.com>
2021-12-09 08:46:22 +0100

    Update UtilisateurManager.java

--- src/fr/eni/zylim/encheres/bll/UtilisateurManager.java
+++ src/fr/eni/zylim/encheres/bll/UtilisateurManager.java
@@ -5,17 +5,17 @@ import java.util.List;
 
 import fr.eni.zylim.encheres.bo.Utilisateur;
 import fr.eni.zylim.encheres.dal.DALException;
+import fr.eni.zylim.encheres.dal.DAOFactory;
 import fr.eni.zylim.encheres.dal.UtilisateurDAO;
-import fr.eni.zylim.encheres.dal.UtilisateurDAOFactory;
 
 // TODO: Auto-generated Javadoc
 /**
  * The Class UtilisateurMAnager.
  */
-public class UtilisateurMAnager {
+public class UtilisateurManager {
 	
 	/** The instance. */
-	private static UtilisateurMAnager instance;
+	private static UtilisateurManager instance;
 
 	/** The dao. */
 	private static UtilisateurDAO dao;
@@ -23,8 +23,8 @@ public class UtilisateurMAnager {
 	/**
 	 * Instantiates a new utilisateur M anager.
 	 */
-	private UtilisateurMAnager() {
-		dao = UtilisateurDAOFactory.getUtilisateurDAO();
+	private UtilisateurManager() {
+		dao = DAOFactory.getUtilisateurDAO();
 	};
 
 	/**
@@ -32,9 +32,9 @@ public class UtilisateurMAnager {
 	 *
 	 * @return single instance of UtilisateurMAnager
 	 */
-	public static UtilisateurMAnager getInstance() {
+	public static UtilisateurManager getInstance() {
 		if (instance == null) {
-			instance = new UtilisateurMAnager();
+			instance = new UtilisateurManager();
 		}
 		return instance;
 	}
