6ebffb8b7ba0f352acc49e0f9bc581688b66ae35
Yanis Chaigneau <yanis.chaigneau.yc2@gmail.com>
2021-12-09 08:30:32 +0100

    push modification DAOFactory

--- src/fr/eni/zylim/encheres/dal/DAOFactory.java
+++ src/fr/eni/zylim/encheres/dal/DAOFactory.java
@@ -1,11 +1,14 @@
 package fr.eni.zylim.encheres.dal;
 
+import fr.eni.zylim.encheres.dal.jdbc.ArticleDAOJdbcImpl;
 import fr.eni.zylim.encheres.dal.jdbc.UtilisateurDAOJdbcImpl;
+
 
 public class DAOFactory {
 	
 	public static UtilisateurDAO getUtilisateurDAO() {
-		return new UtilisateurDAOJdbcImpl();
+		return new UtilisateurDAOJdbcImpl() {
+		};
 	}
 	
 	public static ArticleDAO getArticleDAO() {
